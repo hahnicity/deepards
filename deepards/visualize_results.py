48349e4e57f2b11fb14310818d60fbc1e64477c8
Gregory Rehm <grehm87@gmail.com>
2019-04-28 23:53:09 -0700

    update some plot visualizations

--- deepards/visualize_results.py
+++ deepards/visualize_results.py
@@ -20,6 +20,8 @@ parser.add_argument('-lr', '--learning-rate', type=float, default=.001)
 parser.add_argument('-b', '--batch-size', type=int, default=16)
 args = parser.parse_args()
 
+get_moving_average = lambda x, N: np.convolve(x, np.ones((N,))/N, mode='valid')
+
 all_vals = None
 glob_search = 'results/test_auc*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}*'
 glob_search = glob_search.format(**args.__dict__)
@@ -42,36 +44,27 @@ plt.legend(loc=3)
 plt.grid()
 plt.ylabel('AUC')
 plt.xlabel('epochs')
-plt.ylim(.5, 1)
-plt.yticks(np.arange(.5, 1.01, .05))
+plt.ylim(.45, 1)
+plt.yticks(np.arange(.45, 1.01, .05))
 plt.xticks(range(0, len(all_vals)), range(1, len(all_vals)+1))
 #plt.title(
 plt.show()
 
-all_vals = None
 glob_search = 'results/test_accuracy*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}*'
 glob_search = glob_search.format(**args.__dict__)
 results_files = glob(glob_search)
 if len(results_files) == 0:
-    raise Exception('No results files found')
+    raise Exception('No accuracy results files found')
 
 for i, f in enumerate(sorted(results_files)):
     vals = torch.load(f).values.numpy()
-    vals = np.array([vals.mean()])
-    if all_vals is None:
-        all_vals = vals
-    else:
-        all_vals += vals
-    plt.plot(vals, label='Test accuracy Fold {}'.format(i+1))
-all_vals = all_vals / len(glob(glob_search))
-plt.plot(all_vals, label='Mean Accuracy')
-plt.legend(loc=3)
+    ma = get_moving_average(vals, 100)
+    plt.plot(ma, label='Test accuracy Fold {}'.format(i+1))
+plt.legend()
 plt.grid()
 plt.ylabel('Accuracy')
-plt.xlabel('epochs')
 plt.ylim(.5, 1)
 plt.yticks(np.arange(.5, 1.01, .05))
-plt.xticks(range(0, len(all_vals)), range(1, len(all_vals)+1))
 #plt.title(
 plt.show()
 
@@ -79,12 +72,13 @@ glob_search = 'results/loss*_{network}_base{base_network}_e{epochs}_nb{num_sub_b
 glob_search = glob_search.format(**args.__dict__)
 results_files = glob(glob_search)
 if len(results_files) == 0:
-    raise Exception('No results files found')
+    raise Exception('No loss results files found')
 
 for i, f in enumerate(sorted(results_files)):
     vals = torch.load(f).values.numpy()
-    plt.plot(vals, label='Loss Fold {}'.format(i+1))
-plt.legend(loc=3)
+    ma = get_moving_average(vals, 1000)
+    plt.plot(ma, label='Loss Fold {}'.format(i+1))
+plt.legend()
 plt.grid()
 plt.ylabel('loss')
 plt.show()
