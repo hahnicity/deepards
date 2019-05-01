371178b249b19a573fa0871498e2721a6270f3e9
Gregory Rehm <grehm87@gmail.com>
2019-05-01 01:16:14 -0700

    add rdc to visualizer

--- deepards/visualize_results.py
+++ deepards/visualize_results.py
@@ -18,11 +18,12 @@ parser.add_argument('-lvp', '--lstm-vote-percentage', default=70)
 parser.add_argument('-o', '--optim', default='sgd')
 parser.add_argument('-lr', '--learning-rate', type=float, default=.001)
 parser.add_argument('-b', '--batch-size', type=int, default=16)
+parser.add_argument('-rdc', '--resnet-double-conv', action='store_true')
 args = parser.parse_args()
 
 get_moving_average = lambda x, N: np.convolve(x, np.ones((N,))/N, mode='valid')
 
-glob_search = 'results/loss*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}*'
+glob_search = 'results/loss*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}_rdc{resnet_double_conv}*'
 glob_search = glob_search.format(**args.__dict__)
 results_files = glob(glob_search)
 if len(results_files) == 0:
@@ -38,7 +39,7 @@ plt.ylabel('loss')
 plt.show()
 
 all_vals = None
-glob_search = 'results/test_auc*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}*'
+glob_search = 'results/test_auc*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}_rdc{resnet_double_conv}*'
 glob_search = glob_search.format(**args.__dict__)
 print('searching for results with params:')
 pprint(args.__dict__)
@@ -65,7 +66,7 @@ plt.xticks(range(0, len(all_vals)), range(1, len(all_vals)+1))
 #plt.title(
 plt.show()
 
-glob_search = 'results/test_accuracy*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}*'
+glob_search = 'results/test_accuracy*_{network}_base{base_network}_e{epochs}_nb{num_sub_batches}_lc{loss_calc}_rip{initial_planes}_lvp{lstm_vote_percentage}_rfptmax_optim{optim}_lr{learning_rate}_bs{batch_size}_rdc{resnet_double_conv}*'
 glob_search = glob_search.format(**args.__dict__)
 results_files = glob(glob_search)
 if len(results_files) == 0:
