f5f9829a33fe2009691a260e0a5227bd98de67b9
Gregory Rehm <grehm87@gmail.com>
2019-05-02 15:51:46 -0700

    standardize bm data

--- deepards/dataset.py
+++ deepards/dataset.py
@@ -70,7 +70,29 @@ class ARDSRawDataset(Dataset):
         # again
         self.mu = -0.16998896167389502
         self.std = 25.332015720945343
-
+        flow_time_features = [
+            'mean_flow_from_pef',
+            'inst_RR',
+            'slope_minF_to_zero',
+            'pef_+0.16_to_zero',
+            'iTime',
+            'eTime',
+            'I:E ratio',
+            'dyn_compliance',
+            'tve:tvi ratio',
+        ]
+        # XXX this is kinda hard coded to 9 features even though in the future the #
+        # features will probably change.
+        self.flow_time_bm_mu = [
+            -2.26242758e+01,  4.57935143e+01,  1.06921834e+02,  5.45218237e+01,
+            1.75132215e+00,  3.95646880e+00,  1.02916020e+00,  6.28111796e-02,
+            2.07012537e+00
+        ]
+        self.flow_time_bm_std = [
+            1.76543292e+03, 3.57338569e+03, 8.34414226e+03, 4.25456197e+03,
+            1.36659346e+02, 3.08730757e+02, 8.03104054e+01, 4.90176856e+00,
+            1.61537969e+02
+        ]
         # 224 seems reasonable because it would fit well with existing img systems.
         self.seq_len = 224
         if dataset_type == 'padded_breath_by_breath':
@@ -86,19 +108,7 @@ class ARDSRawDataset(Dataset):
         elif dataset_type == 'padded_breath_by_breath_with_limited_bm target':
             self._get_breath_by_breath_with_breath_meta_target(self._pad_breath, ['iTime', 'eTime', 'inst_RR'])
         elif dataset_type == 'padded_breath_by_breath_with_flow_time_features':
-            self._get_breath_by_breath_with_flow_time_features(
-                self._pad_breath, [
-                    'mean_flow_from_pef',
-                    'inst_RR',
-                    'slope_minF_to_zero',
-                    'pef_+0.16_to_zero',
-                    'iTime',
-                    'eTime',
-                    'I:E ratio',
-                    'dyn_compliance',
-                    'tve:tvi ratio',
-                ]
-            )
+            self._get_breath_by_breath_with_flow_time_features(self._pad_breath, flow_time_features)
 
         if to_pickle:
             pd.to_pickle(self.all_sequences, to_pickle)
@@ -166,6 +176,7 @@ class ARDSRawDataset(Dataset):
                 if np.any(np.abs(meta[ratio_indices]) > 100):
                     continue
 
+                meta = (meta - self.flow_time_bm_mu) / self.flow_time_bm_std
                 flow = (np.array(breath['flow']) - self.mu) / self.std
                 b_seq = process_breath_func(flow)
                 batch_arr.append(b_seq)
