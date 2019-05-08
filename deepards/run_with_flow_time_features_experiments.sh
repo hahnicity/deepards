d9654332ab43a5694b01d3451b1cd3f0133c23f8
Gregory Rehm <grehm87@gmail.com>
2019-05-08 01:50:24 -0700

    update with flow time feature experiment to not run a variety of input planes

--- deepards/run_with_flow_time_features_experiments.sh
+++ deepards/run_with_flow_time_features_experiments.sh
@@ -6,9 +6,9 @@ do
     do
         for lr in .001
         do
-            for rip in 8 16 64
+            for rip in 64
             do
-                for bs in 16 32
+                for bs in 8 16 32
                 do
                     ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath_with_flow_time_features-nb100-kfolds.pkl -dt  padded_breath_by_breath_with_flow_time_features -n cnn_lstm --cuda -rip ${rip} -b ${bs} -e 15 --no-print-progress --kfolds 5
                     ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath_with_flow_time_features-nb100-kfolds.pkl -dt  padded_breath_by_breath_with_flow_time_features -n cnn_lstm --cuda -rip ${rip} -b ${bs} -e 15 --no-print-progress --kfolds 5 --bm-to-linear
