01fc43e5b1a5bbe5074f8f14b23e1bb41b6f95b7
Gregory Rehm <grehm87@gmail.com>
2019-05-08 23:25:23 -0700

    add script for creating train/test split for cohort metadata set

---
+++ deepards/create_breath_meta_dataset_split.py
@@ -0,0 +1,54 @@
+import argparse
+from glob import glob
+import os
+import subprocess
+
+import pandas as pd
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset_dir')
+args = parser.parse_args()
+
+cohort = pd.read_csv('cohort-description.csv')
+ards_cohort_patients = cohort[(cohort.experiment_group == 1) & (cohort['Potential Enrollment'] == 'Y')]['Patient Unique Identifier']
+
+all_data_raw_path = os.path.join(args.dataset_dir, 'experiment1', 'all_data', 'raw')
+all_data_meta_path = os.path.join(args.dataset_dir, 'experiment1', 'all_data', 'meta')
+training_raw_path = os.path.join(args.dataset_dir, 'experiment1', 'training', 'raw')
+training_meta_path = os.path.join(args.dataset_dir, 'experiment1', 'training', 'meta')
+testing_raw_path = os.path.join(args.dataset_dir, 'experiment1', 'testing', 'raw')
+testing_meta_path = os.path.join(args.dataset_dir, 'experiment1', 'testing', 'meta')
+
+try:
+    os.makedirs(training_raw_path)
+except OSError:
+    pass
+try:
+    os.makedirs(training_meta_path)
+except OSError:
+    pass
+try:
+    os.makedirs(testing_raw_path)
+except OSError:
+    pass
+try:
+    os.makedirs(testing_meta_path)
+except OSError:
+    pass
+
+all_patients = [os.path.basename(i) for i in glob(all_data_raw_path + '/*')]
+train_cohort = set(all_patients).difference(set(ards_cohort_patients))
+test_cohort = set(all_patients).intersection(set(ards_cohort_patients))
+
+for patient in train_cohort:
+    proc = subprocess.Popen(['ln', '-s', all_data_raw_path + "/" + patient, training_raw_path])
+    proc.communicate()
+    proc = subprocess.Popen(['ln', '-s', all_data_meta_path + "/" + patient, training_meta_path])
+    proc.communicate()
+
+for patient in test_cohort:
+    proc = subprocess.Popen(['ln', '-s', all_data_raw_path + "/" + patient, testing_raw_path])
+    proc.communicate()
+    proc = subprocess.Popen(['ln', '-s', all_data_meta_path + "/" + patient, testing_meta_path])
+    proc.communicate()
