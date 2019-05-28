955d11230ce0cc8606cd2e8bdc4eec01aa6e7406
Gregory Rehm <grehm87@gmail.com>
2019-05-28 10:33:15 -0700

    update pretraining experiments for expanded BM dataset

--- deepards/run_pretrained_models_with_metadata_experiments.sh
+++ deepards/run_pretrained_models_with_metadata_experiments.sh
@@ -8,9 +8,9 @@ do
         do
             for rip in 64
             do
-                for bs in 8 16 32
+                for bs in 8 16 32 64
                 do
-                    ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath-nb100-kfold.pkl -dt padded_breath_by_breath -n cnn_lstm --cuda -rip ${rip} -b ${bs} --load-pretrained resnet18-bm-limited-rip64-bs32.pth -e 15  --no-print-progress --kfolds 5 -exp with_pretrained_models
+                    ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath-nb100-kfold.pkl -dt padded_breath_by_breath -n cnn_lstm --cuda -rip ${rip} -b ${bs} --load-pretrained resnet18-bm-limited-c50-b500-rip64-bs32.pth -e 15  --no-print-progress --kfolds 5 -exp with_pretrained_models_c50_b500
                 done
             done
         done
