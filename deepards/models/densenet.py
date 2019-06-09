ef2575f5d2a6b815d6ad8398ddbc1440af30096c
Bhargav <bhargavsundararajan@github.com>
2019-06-08 23:23:24 -0700

    debug densenet

--- deepards/models/densenet.py
+++ deepards/models/densenet.py
@@ -119,6 +119,7 @@ class DenseNet(nn.Module):
 
     def forward(self, x):
         features = self.features(x)
+        print(features.size())
         out = F.relu(features, inplace=True)
         out = self.avgpool(out)
         out = out.view(features.size(0), -1)
