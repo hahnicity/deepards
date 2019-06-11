2616fa8cd42133366182cc6213189adcb9ea4788
Kavish <kavishdoshi28@gmail.com>
2019-06-11 15:26:43 -0700

    deepnet arch change

--- deepards/models/densenet.py
+++ deepards/models/densenet.py
@@ -70,7 +70,7 @@ class DenseNet(nn.Module):
         num_classes (int) - number of classification classes
     """
 
-    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
+    def __init__(self, growth_rate=32, block_config=(2, 2, 2, 2),
                  num_init_features=64, bn_size=4, drop_rate=0.2, num_classes=1000):
 
         super(DenseNet, self).__init__()
