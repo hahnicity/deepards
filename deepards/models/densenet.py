fdd8e6b060e36d8599fd89a601fab42d6cf4c196
Kavish <kavishdoshi28@gmail.com>
2019-06-11 13:22:07 -0700

    debug densenet

--- deepards/models/densenet.py
+++ deepards/models/densenet.py
@@ -106,9 +106,9 @@ class DenseNet(nn.Module):
 
         # Official init from torch repo.
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
+            if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, nn.BatchNorm1d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.Linear):
