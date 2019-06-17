b6de4599a3cf458396b0ee2b84cd46c9605b23d2
Gregory Rehm <grehm87@gmail.com>
2019-06-17 14:44:07 -0700

    ensure we dont use unet for pretraining

--- deepards/run_autoencoder_pretraining.sh
+++ deepards/run_autoencoder_pretraining.sh
@@ -4,6 +4,6 @@ for nb in 20 40
 do
 	for bs in 8 16 32
 	do
-		ts python train_ards_detector.py -dt unpadded_downsampled_autoencoder_sequences --no-drop-frames -nb ${nb} --train-to-pickle unpadded_downsampled_autoencoder_sequences-nb${nb}-no-drop-train.pkl --test-to-pickle unpadded_downsampled_autoencoder_sequences-nb${nb}-no-drop-test.pkl -n autoencoder --base-network unet -dp /fastdata/autoencoder_dataset_b64 --cuda -b ${bs} --no-print-progress --save-model autoencoder-pretrained-b64-nb${nb}-bs${bs}.pth --downsample-factor 4 -exp autoencoder_pretraining -e 15
+		ts python train_ards_detector.py -dt unpadded_downsampled_autoencoder_sequences --no-drop-frames -nb ${nb} --train-to-pickle unpadded_downsampled_autoencoder_sequences-nb${nb}-no-drop-train.pkl --test-to-pickle unpadded_downsampled_autoencoder_sequences-nb${nb}-no-drop-test.pkl -n autoencoder --base-network basic_cnn_ae -dp /fastdata/autoencoder_dataset_b64 --cuda -b ${bs} --no-print-progress --save-model autoencoder-pretrained-b64-nb${nb}-bs${bs}.pth --downsample-factor 4 -exp autoencoder_pretraining -e 15
 	done
 done
