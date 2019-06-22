d08ba833425ae081342d52255df8129d65871520
Gregory Rehm <grehm87@gmail.com>
2019-06-22 15:56:10 -0700

    fix bm regressor for models other than resnet18

--- deepards/models/torch_cnn_bm_regressor.py
+++ deepards/models/torch_cnn_bm_regressor.py
@@ -9,11 +9,11 @@ class CNNRegressor(nn.Module):
 
         self.seq_size = 224
         self.breath_block = breath_block
-        self.linear_final = nn.Linear(breath_block.inplanes, n_final_features)
+        self.linear_final = nn.Linear(breath_block.n_out_filters, n_final_features)
 
     def forward(self, x, metadata):
         # input should be in shape: (batches, breaths in seq, chans, 224)
         if x.shape[-1] != 224:
             raise Exception('input breaths must have sequence length of 224')
-        outputs = self.linear_final(self.breath_block(x))
+        outputs = self.linear_final(self.breath_block(x).squeeze())
         return outputs
