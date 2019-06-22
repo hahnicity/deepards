414d3cfa15345611223380df49773e5497ec238c
Gregory Rehm <grehm87@gmail.com>
2019-06-22 15:22:46 -0700

    ugh.. stupid bug invalidated days of runs

--- deepards/run_limited_breath_meta_pretraining_regressors.sh
+++ deepards/run_limited_breath_meta_pretraining_regressors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for base_network in 'senet18' 'senet154' 'se_resnet18' 'se_reset50' 'se_resnext50_32x4d'
+for base_network in 'densenet18' 'densenet161' 'densenet201' 'senet18' 'senet154' 'se_resnet18' 'se_reset50' 'se_resnext50_32x4d'
 do
-    for bs in 64 128 256
+    for bs in 128
     do
-        ts python train_ards_detector.py --test-from-pickle /fastdata/deepards/padded_breath_by_breath_with_limited_bm_new-bm-dataset-c50-b500-test.pkl --train-from-pickle /fastdata/deepards/padded_breath_by_breath_with_limited_bm_new-bm-dataset-c50-b500-train.pkl -dt padded_breath_by_breath_with_limited_bm_target -n cnn_regressor --cuda -b ${bs} -e 10 --no-print-progress --save-model ${base_network}-bm-limited-c50-b500-bs${bs}.pth
+        ts python train_ards_detector.py --test-from-pickle /fastdata/deepards/padded_breath_by_breath_with_limited_bm_new-bm-dataset-c50-b500-test.pkl --train-from-pickle /fastdata/deepards/padded_breath_by_breath_with_limited_bm_new-bm-dataset-c50-b500-train.pkl -dt padded_breath_by_breath_with_limited_bm_target -n cnn_regressor --cuda -b ${bs} -e 10 --no-print-progress --save-model ${base_network}-bm-limited-c50-b500-bs${bs}.pth --base-network ${base_network}
     done
 done
--- deepards/run_pretrained_models_with_metadata_experiments.sh
+++ deepards/run_pretrained_models_with_metadata_experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for network in 'cnn_lstm'
+for network in 'cnn_linear' 'cnn_lstm'
 do
-    for base_network in 'senet18' 'senet154' 'se_resnet18' 'se_reset50' 'se_resnext50_32x4d'
+    for base_network in 'densenet18' 'densenet161' 'densenet201'
     do
         for bs in 16
         do
-            ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath-nb100-kfold.pkl -dt padded_breath_by_breath -n $network --cuda -b ${bs} --load-pretrained ${base_network}-bm-limited-c50-b500-bs128.pth -e 10  --no-print-progress --kfolds 5 -exp with_senet_pretrained_models_c50_b500
+            ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath-nb100-kfold.pkl -dt padded_breath_by_breath -n $network --cuda -b ${bs} --load-pretrained ${base_network}-bm-limited-c50-b500-bs128.pth -e 10  --no-print-progress --kfolds 5 -exp with_densenet_pretrained_models_c50_b500
         done
     done
 done
