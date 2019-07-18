6b544d831288b68c89a6229f5177907c280f4e21
Gregory Rehm <grehm87@gmail.com>
2019-07-18 15:29:07 -0700

    fix 2d issue on vgg

--- deepards/models/vgg.py
+++ deepards/models/vgg.py
@@ -12,37 +12,25 @@ class VGG(nn.Module):
     def __init__(self, features, num_classes=1000, init_weights=True):
         super(VGG, self).__init__()
         self.features = features
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
+        self.avgpool = nn.AdaptiveAvgPool1d(7)
         if init_weights:
             self._initialize_weights()
+        self.n_out_filters = 512 * 7
 
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        #x = self.classifier(x)
         return x
 
     def _initialize_weights(self):
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
+            if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, nn.BatchNorm1d):
                 nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.constant_(m.bias, 0)
 
 
@@ -51,7 +39,7 @@ def make_layers(cfg, batch_norm=False):
     in_channels = 1
     for v in cfg:
         if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            layers += [nn.MaxPool1d(kernel_size=2, stride=2)]
         else:
             conv1d = nn.Conv1d(in_channels, v, kernel_size=3, padding=1)
             if batch_norm:
