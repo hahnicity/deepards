d434626a90dcb0cd2b0f9277e69a31f329d56bfa
Gregory Rehm <grehm87@gmail.com>
2019-07-26 00:36:33 -0700

    redo some sensitivity analyses

--- deepards/experiment_scripts/run_pretrained_models_with_confidence_penalty.sh
+++ deepards/experiment_scripts/run_pretrained_models_with_confidence_penalty.sh
@@ -10,7 +10,7 @@ do
             do
                 for beta in .25 .5 1 2 4
                 do
-                    ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath-nb100-kfold.pkl -dt padded_breath_by_breath -n cnn_lstm --cuda -b ${bs} --load-pretrained resnet18-bm-limited-c50-b500-rip64-bs32.pth -e 10  --no-print-progress --kfolds 5 -exp confidence_penalty2 -loss confidence --conf-beta $beta
+                    ts python train_ards_detector.py -p /fastdata/deepards/padded_breath_by_breath-nb100-kfold.pkl -dt padded_breath_by_breath -n cnn_lstm --cuda -b ${bs} --load-pretrained resnet-c50-b500-exprimental3.pth -e 5 --no-print-progress --kfolds 5 -exp confidence_penalty2 -loss confidence --conf-beta $beta
                 done
             done
         done
--- deepards/experiment_scripts/run_unpadded_sequences_sub_batch_search.sh
+++ deepards/experiment_scripts/run_unpadded_sequences_sub_batch_search.sh
@@ -8,11 +8,11 @@ do
         do
             for rip in 64
             do
-                for bs in 16 32
+                for bs in 16
                 do
-                    for nb in 75 150 225
+                    for nb in 25 50 75 100 150 225 300
                     do
-                        ts python train_ards_detector.py --train-to-pickle unpadded_sequences-nb${nb}-kfold.pkl -dt unpadded_sequences -n cnn_lstm -nb ${nb} --cuda -rip ${rip} -b ${bs} -e 15 --no-print-progress --kfolds 5 -dp /fastdata/ardsdetection --loader-threads 4 -exp unpadded_sequences_sub_batch_search
+                        ts python train_ards_detector.py --train-to-pickle unpadded_sequences-nb${nb}-kfold.pkl -dt unpadded_sequences -n cnn_lstm -nb ${nb} --cuda -rip ${rip} -b ${bs} -e 10 --no-print-progress --kfolds 5 -dp /fastdata/ardsdetection --loader-threads 4 -exp unpadded_sequences_sub_batch_search2
                     done
                 done
             done
