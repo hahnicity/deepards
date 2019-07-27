4550791dd58c9dd8cafdfa6be013c2771201e6db
sherlockof96 <kavish@localhost.localdomain>
2019-07-26 20:51:58 -0700

    all patient

--- deepards/debug_file.py
+++ deepards/debug_file.py
@@ -5,10 +5,25 @@ import numpy as np
 
 
 data = pickle.load( open( "pickledata1.pkl", "rb" ) )
-#print(len(data))
+print(len(data))
 #print(data[0])
 #0015RPI0320150401 : example for patient id
+ards = np.zeros(shape=(0))
+non_ards = np.zeros(shape=(0))
+count = 0
 for i in range(len(data)):
-    print(type(data[i][0])) 
-    print(data[i+1][0])
-    break
+    
+    if data[i][2][1] == 1:
+        count = count + 1
+        if data[i][0] not in ards:
+            ards = np.append(ards,data[i][0])
+    
+    elif data[i][2][1] == 0:
+        count = count + 1
+        if data[i][0] not in non_ards:
+            non_ards = np.append(non_ards,data[i][0])
+
+#print(ards)
+#print(non_ards)
+print(count)
+   
--- deepards/patient_gradcam.py
+++ deepards/patient_gradcam.py
@@ -13,53 +13,26 @@ import cv2
 #importing the class for calculating the cam values
 from gradcam import GradCam
 
+def get_ardsids_otherids(data):
+    ards = np.zeros(shape=(0))
+    non_ards = np.zeros(shape=(0))
+    for i in range(len(data)):
 
-def visualize_sequence(br, gradcam):
-    #inizializing the image variable to 224 pickles with 0
-    img = np.zeros((224,1))
-    br = np.expand_dims(br, axis = 0)
-    #since the br is in 1 x 224 format we are transfering it into 224 x 1 format
-    for i in range(0,224):
-        img[i][0] = br[0][i]
-    #arranging the x-axis since the data is of 224
-    dt = 1
-    t = np.arange(0, 224, dt)
+        if data[i][2][1] == 1:
+            if data[i][0] not in ards:
+                ards = np.append(ards,data[i][0])
 
-    #plotting the imageto scalar va
-    plt.scatter(t, img,c=gradcam,vmin = 0 , vmax = 255)
-    plt.plot(t,img)
-    cbar  = plt.colorbar()
-    cbar.set_label("gradcam", labelpad=-1)
-    plt.show()
+        elif data[i][2][1] == 0:
+            if data[i][0] not in non_ards:
+                non_ards = np.append(non_ards,data[i][0])
 
+    return ards, non_ards
 
-if __name__ == '__main__':
-
-    #space for putting all the required parsers
-    parser = argparse.ArgumentParser()
-    #add parser for pickle data
-    #add parser for patient id
-
-    #initializing the variables
-    
-    #0015RPI0320150401 : example for patient id
-    patient_id = '0015RPI0320150401'
-    #Getting the pretrained model
-    PATH = 'densenet-linear-model--fold-3.pth'
-    #for avg breath original
+def get_avgbre_camout(patient_id, data,pretrained_model):
+    #initializing variables
     avg_breath = np.empty((0,224))
-    pretrained_model = torch.load(PATH)
-    target_class = None
     cam_outputs = np.empty((0,7))
-
-    #loading the data
-
-    data = pickle.load( open( "pickledata1.pkl", "rb" ) )
-    #print(len(data))
-    #print(data[0])
-
-    #itterating through the data to get the sequences of all the breaths of the particular patients
-
+    target_class = None
     for i in range(len(data)):
         if patient_id == data[i][0]:
             breath_sequence = data[i][1]
@@ -79,6 +52,78 @@ if __name__ == '__main__':
     #print(cam_outputs)
     #print(len(cam_outputs))
     #print(cam_outputs.shape)
+    return avg_breath,cam_outputs
 
-    #for visualizing the sequence for a patient id
-    visualize_sequence(avg_breath,cam_outputs)
+
+def visualize_sequence(br, gradcam,patient_id,c):
+    #inizializing the image variable to 224 pickles with 0
+    img = np.zeros((224,1))
+    br = np.expand_dims(br, axis = 0)
+    #since the br is in 1 x 224 format we are transfering it into 224 x 1 format
+    for i in range(0,224):
+        img[i][0] = br[0][i]
+    #arranging the x-axis since the data is of 224
+    dt = 1
+    t = np.arange(0, 224, dt)
+
+    #plotting the imageto scalar va
+    plt.scatter(t, img,c=gradcam,vmin = 0 , vmax = 255)
+    plt.plot(t,img)
+    cbar  = plt.colorbar()
+    cbar.set_label("gradcam", labelpad=-1)
+    plt.title(patient_id)
+    if c == 1:
+        filename = 'gradcam_results' + '/ards/' + patient_id + '.png'
+    else:
+        filename = 'gradcam_results' + '/non_ards/' + patient_id + '.png'
+    plt.savefig(filename)
+    plt.close()
+    #plt.show()
+
+
+if __name__ == '__main__':
+
+    #space for putting all the required parsers
+    parser = argparse.ArgumentParser()
+    #add parser for pickle data
+    parser.add_argument('-pd', '--pickled-data', help = 'PATH to pickled data')
+    args = parser.parse_args()
+    #loading the data
+    data_filename = args.pickled_data
+    data = pickle.load( open( data_filename, "rb" ) )
+    #print(len(data))
+    #print(data[0])
+
+    #initializing the variables
+
+    #Getting the pretrained model
+    PATH = 'densenet-linear-model--fold-3.pth'
+    pretrained_model = torch.load(PATH)
+    #0015RPI0320150401 : example for patient id
+    patient_id = '0015RPI0320150401'
+    
+    #getting all ardsids and nonards ids:
+    ards,nonards = get_ardsids_otherids(data)
+    c = 1
+    
+    #for ards patients
+    for i in range(len(ards)):
+        print(i)
+        patient_id = ards[i]
+        c = 1
+        #itterating through the data to get the sequences of all the breaths of the particular patients
+        avg_br,cam_out = get_avgbre_camout(patient_id,data,pretrained_model)
+    
+        #for visualizing the sequence for a patient id
+        visualize_sequence(avg_br,cam_out,patient_id,c)
+    
+    #for non_ards patients
+    for i in range(len(nonards)):
+        print(i)
+        patient_id = nonards[i]
+        c = 0
+        #itterating through the data to get the sequences of all the breaths of the particular patients
+        avg_br,cam_out = get_avgbre_camout(patient_id,data,pretrained_model)
+
+        #for visualizing the sequence for a patient id
+        visualize_sequence(avg_br,cam_out,patient_id,c)
