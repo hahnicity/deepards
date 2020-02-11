3d07458668c5d59a30943c03be903f2fe10a2226
Gregory Rehm <grehm87@gmail.com>
2020-02-10 22:56:54 -0800

    make sure that we can adjust grad clip size and that n sub batches is reflected properly when we run main script

--- deepards/scripts/main/run_non_pretraining_experiments.py
+++ deepards/scripts/main/run_non_pretraining_experiments.py
@@ -12,17 +12,17 @@ n_times_each_experiment = 10
 grad_clip = .01
 
 
-def run_experiment(dataset_path, network, bs, epochs, kfolds, base_network, weight_decay, dataset_type, dry_run, experiment_name_prefix):
+def run_experiment(dataset_path, network, bs, epochs, kfolds, base_network, weight_decay, dataset_type, dry_run, experiment_name_prefix, n_sub_batches, clip_val):
     experiment_name = "{}_{}_{}_{}".format(experiment_name_prefix, dataset_type, network, base_network)
-    command = [
+    command = [str(i) for i in [
         'ts', 'python', 'train_ards_detector.py', '--train-from-pickle', dataset_path,
         '-n', network, '--cuda', '-b', bs, '-e', epochs, '--no-print-progress',
         '--kfolds', kfolds, '-exp', experiment_name, '--base-network', base_network,
         '--oversample', '-wd', weight_decay, '-dt', dataset_type, '--clip-grad', '--clip-val',
-        grad_clip,
-    ]
+        clip_val, '-nb', n_sub_batches,
+    ]]
     if dry_run:
-        print("Running:\n\n{}".format(" ".join([str(i) for i in command])))
+        print("Running:\n\n{}".format(" ".join(command)))
         return
 
     for i in range(n_times_each_experiment):
@@ -37,12 +37,13 @@ def main():
     parser.add_argument('-b', '--base-networks', nargs='+', choices=base_networks.keys(), required=True)
     parser.add_argument('--dry-run', action='store_true')
     parser.add_argument('-e', '--experiment-name', required=True)
+    parser.add_argument('--clip-val', type=float, default=.01)
     args = parser.parse_args()
 
-    for dataset_type, dataset_path in [
-       ('unpadded_sequences', '/fastdata/deepards/unpadded_sequences-nb150-kfold.pkl'),
-       ('unpadded_centered_sequences', '/fastdata/deepards/unpadded_centered_sequences-nb20-kfold.pkl'),
-       ('unpadded_downsampled_sequences', '/fastdata/deepards/unpadded_downsampled_sequences-nbXXX-kfold.pkl')
+    for dataset_type, dataset_path, n_sub_batches in [
+       ('unpadded_sequences', '/fastdata/deepards/unpadded_sequences-nb150-kfold.pkl', 150),
+       ('unpadded_centered_sequences', '/fastdata/deepards/unpadded_centered_sequences-nb20-kfold.pkl', 20),
+       ('unpadded_downsampled_sequences', '/fastdata/deepards/unpadded_downsampled_sequences-nbXXX-kfold.pkl', 'XXX')
     ]:
         if dataset_type not in args.dataset_type:
             continue
@@ -50,10 +51,10 @@ def main():
         for network in args.networks:
             if network == 'lstm_only':
                 # the base network input doesnt matter here
-                run_experiment(dataset_path, network, bs, epochs, kfolds, 'resnet18', weight_decay, dataset_type, args.dry_run, args.experiment_name)
+                run_experiment(dataset_path, network, bs, epochs, kfolds, 'resnet18', weight_decay, dataset_type, args.dry_run, args.experiment_name, n_sub_batches, args.clip_val)
             else:
                 for base_network in args.base_networks:
-                    run_experiment(dataset_path, network, bs, epochs, kfolds, base_network, weight_decay, dataset_type, args.dry_run, args.experiment_name)
+                    run_experiment(dataset_path, network, bs, epochs, kfolds, base_network, weight_decay, dataset_type, args.dry_run, args.experiment_name, n_sub_batches, args.clip_val)
 
 
 if __name__ == "__main__":
