884b41dd6f0973e3c4d9ebcf5ed945f1e7de2506
Gregory Rehm <grehm87@gmail.com>
2020-04-08 02:58:46 -0700

    add fractional training experiment files

--- deepards/scripts/main/run_non_pretraining_experiments.py
+++ deepards/scripts/main/run_non_pretraining_experiments.py
@@ -2,34 +2,39 @@ import argparse
 import subprocess
 
 
-def run_experiment(dry_run, experiment_name_prefix, cuda_arg, config_override, cuda_device):
+def run_experiment(dry_run, experiment_name_prefix, cuda_arg, config_override, cuda_devices):
     experiment_name = "{}_{}".format(experiment_name_prefix, config_override.split('.yml')[0].replace('/', '_'))
-    command = [str(i) for i in [
+    commands = [[str(i) for i in [
         'ts', 'python', 'train_ards_detector.py', '-co', config_override,
         '--no-print-progress', '-exp', experiment_name,
         '--oversample', '--clip-grad', cuda_arg,
-        '--cuda-device', cuda_device,
-    ]]
+        '--cuda-device', dev,
+    ]] for dev in cuda_devices.split('+')]
 
     if dry_run:
-        print("Running:\n\n{}".format(" ".join(command)))
-        return
+        print('\nDry Runnings:\n')
 
     n_times_each_experiment = 10
-    for i in range(n_times_each_experiment):
-        proc = subprocess.Popen(command)
-        proc.communicate()
+    i = 0
+    while i < n_times_each_experiment:
+        to_run = commands[i % len(commands)]
+        if dry_run:
+            print("{}\n".format(" ".join(to_run)))
+        else:
+            proc = subprocess.Popen(to_run)
+            proc.communicate()
+        i += 1
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dry-run', action='store_true')
     parser.add_argument('-e', '--experiment-name', required=True)
-    parser.add_argument('--cuda-device', type=int, default=0)
+    parser.add_argument('--cuda-devices', help="cuda device you wish to use. Can use + to split work across different devices. E.g. 0+1", required=True)
     parser.add_argument('-co', '--config-override', required=True, help='Path to config override file you set for the experiment')
     args = parser.parse_args()
 
-    run_experiment(args.dry_run, args.experiment_name, '--cuda-no-dp', args.config_override, args.cuda_device)
+    run_experiment(args.dry_run, args.experiment_name, '--cuda-no-dp', args.config_override, args.cuda_devices)
 
 
 if __name__ == "__main__":
