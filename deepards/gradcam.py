fix bug in gradcam where we didnt have relu and were using softmax

deepards/gradcam.py
@@ -7,6 +7,7 @@ import torch
 import argparse
 import pickle
 import cv2
+import torch.nn.functional as F
 
 #from breath_visualize import visualize_sequence
 
@@ -41,19 +42,16 @@ class CamExtractor():
 
     def forward_pass(self, x):
         """
-            Does a full forward pass on the model
+        Does a full forward pass on the model
         """
         #print(x.shape)
         # Forward pass on the convolutions
         conv_output, x = self.forward_pass_on_convolutions(x)
-        #print(conv_output.shape)
-        #print(x.shape)
-        x = self.model.breath_block.avgpool(x)
-        #print(x.shape)
-        x = x.view(-1)  # Flatten
-        # Forward pass on the classifier
+        # they forgot relu when they were doing this initially. damn
+        x = F.relu(x)
+        # pool and flatten
+        x = self.model.breath_block.avgpool(x).view(-1)
         x = self.model.linear_final(x).unsqueeze(0)
-        x = self.softmax(x)
         return conv_output, x
 
 
@@ -77,53 +75,28 @@ class GradCam():
             target_class = np.argmax(model_output.cpu().data.numpy())
         # Target for backprop
         one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
-        #print(one_hot_output.shape)
         one_hot_output[0][target_class] = 1
-        # Zero grads
         self.model.zero_grad()
-        #self.model.classifier.zero_grad()
         # Backward pass with specified target
         model_output.backward(gradient=one_hot_output.cuda(), retain_graph=True)
         # Get hooked gradients
         guided_gradients = self.extractor.gradients.cpu().data.numpy()
-        #print(guided_gradients)
         # Get convolution outputs
         target = conv_output.cpu().data.numpy()
-        #print(guided_gradients.shape)
-        #print(target.shape[1:])
-        # Get weights from gradients
-        #weights = np.mean(guided_gradients, axis = 0)
-        #print(weights[0])
-        weights = np.mean(guided_gradients, axis = (0,2))
-        #print(weights[0])
-        target = np.mean(target, axis = 0)  # Take averages for each gradient
-        # Create empty numpy array for cam
+        weights = np.mean(guided_gradients, axis=(0, 2))
+        # Take averages across all breaths because of the way we are structuring
+        # our model
+        target = np.mean(target, axis=0)
         cam = np.ones(target.shape[1:], dtype=np.float32)
-        #print(weights.shape)
-        #print(target[:,1, :].shape)
-        #print(cam.shape)
         # Multiply each weight with its conv output and then, sum
         for i, w in enumerate(weights):
-            cam += w * target[i,:]
-
+            cam += w * target[i, :]
 
         #cam = np.mean(cam, axis = 0)
         cam = np.maximum(cam, 0)
         cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))  # Normalize between 0-1
         cam = np.uint8(cam * 255)  # Scale between 0-255 to visualize
-        #print(cam)
 
-        '''
-        cam = np.uint8(Image.fromarray(cam).resize((input_image.shape[2],
-                       input_image.shape[3]), Image.ANTIALIAS))/255
-        # ^ I am extremely unhappy with this line. Originally resizing was done in cv2 which
-        # supports resizing numpy matrices with antialiasing, however,
-        # when I moved the repository to PIL, this option was out of the window.
-        # So, in order to use resizing with ANTIALIAS feature of PIL,
-        # I briefly convert matrix to PIL image and then back.
-        # If there is a more beautiful way, do not hesitate to send a PR.
-        return cam
-        '''
         return cam
 
 

deepards/patient_gradcam.py
@@ -32,6 +32,7 @@ class PatientGradCam(object):
         self.gt = self.data.get_ground_truth_df()
         self.ards, self.non_ards = self.get_ardsids_otherids()
         self.batch_size = self.data.all_sequences[0][1].shape[0]
+        self.breath_len = 224
 
     def get_ardsids_otherids(self):
         ards = self.gt[self.gt.y == 1].patient.unique()
@@ -47,7 +48,11 @@ class PatientGradCam(object):
         do_makedirs(dirname)
 
         # XXX in future make this configurable
-        med_breath = np.empty((0, self.batch_size, 1, 224))
+        #
+        # Also, why do cam outputs have to be 7 long?????? XXX
+        # maybe trying to strike a balance with precision of results. Too many outputs
+        # and it becomes too noisy, too few and its not granular enough.
+        med_breath = np.empty((0, self.batch_size, 1, self.breath_len))
         cam_outputs = np.empty((0,7))
         target_class = None
         for i in patient_idxs:
@@ -58,7 +63,7 @@ class PatientGradCam(object):
         med_breath = np.expand_dims(med_breath, axis=0)
         br = torch.FloatTensor(med_breath)[[0] * self.batch_size].cuda()
         cam = self.grad_cam.generate_cam(br, target)
-        cam_outputs = cv2.resize(cam, (1,224))
+        cam_outputs = cv2.resize(cam, (1, self.breath_len))
         filename = os.path.join(dirname, patient_id + '.png')
         self.visualize_sequence(med_breath[0], cam_outputs, patient_id, target, filename)
 
@@ -70,7 +75,7 @@ class PatientGradCam(object):
         dirname = os.path.join('gradcam_results', 'patient_averages', mapping[target])
         do_makedirs(dirname)
 
-    	avg_breath = np.empty((0,224))
+    	avg_breath = np.empty((0, self.breath_len))
     	cam_outputs = np.empty((0,7))
 
         for i in patient_idxs:
@@ -84,7 +89,7 @@ class PatientGradCam(object):
             cam = np.expand_dims(cam, axis = 0)
             cam_outputs = np.append(cam_outputs, cam, axis = 0)
         cam_outputs = np.mean(cam_outputs, axis = 0)
-        cam_outputs = cv2.resize(cam_outputs,(1,224))
+        cam_outputs = cv2.resize(cam_outputs,(1, self.breath_len))
         avg_breath = np.mean(avg_breath, axis = 0)
         filename = os.path.join(dirname, patient_id + '.png')
         self.visualize_sequence(avg_breath, cam_outputs, patient_id, target, filename)
@@ -102,15 +107,16 @@ class PatientGradCam(object):
         dirname = os.path.join('gradcam_results', 'sampled_sequences', mapping[target], patient_id)
         do_makedirs(dirname)
 
-        for i in patient_idxs:
+        for abs_idx in patient_idxs:
             rand_seq = random.choice(range(self.batch_size))
-            cam_outputs, br = self.get_single_sequence_grad_cam(i, rand_seq, target)
-            filename = os.path.join(dirname, 'seq-{}-{}.png'.format(i, rand_seq))
-            self.visualize_sequence(breath_sequence[0], cam_outputs, patient_id, target, filename)
+            rel_idx = list(self.data.kfold_indexes).index(abs_idx)
+            cam_outputs, br = self.get_single_sequence_grad_cam(rel_idx, rand_seq, target)
+            filename = os.path.join(dirname, 'seq-{}-{}.png'.format(abs_idx, rand_seq))
+            self.visualize_sequence(br[0], cam_outputs, patient_id, target, filename)
 
     def plot_sequence(self, br, cam_outputs):
-        img = br.reshape((len(br), 1))
-        t = np.arange(0, len(br), 1)
+        img = br.reshape((self.breath_len, 1))
+        t = np.arange(0, self.breath_len, 1)
         plt.scatter(t, img, c=cam_outputs, vmin = 0, vmax = 255)
         plt.plot(t, img)
 
@@ -129,27 +135,35 @@ class PatientGradCam(object):
         elif len(br.shape) == 2:
             br = np.expand_dims(br, axis=0)
 
+        # make sure the breath is duplicated <batch_size> times
+        #
+        # does the duplication mess things up? thats my main worry here. I think
+        # another worry of mine is that grad cam outputs dont translate one to one
+        # with an actual input. because there are multiple inputs that are being run
+        # and then classified.
         br = torch.FloatTensor(br)[[0] * self.batch_size].cuda()
         cam = self.grad_cam.generate_cam(br, target)
-        cam_outputs = cv2.resize(cam, (1,224))
+        cam_outputs = cv2.resize(cam, (1, self.breath_len))
         return cam_outputs, br.cpu().numpy()
 
     def get_single_sequence_grad_cam(self, seq_idx, batch_idx, target):
+        # this op could be a time hog
         item = self.data[seq_idx]
         br = np.expand_dims(item[1][batch_idx], axis=0)
         return self.get_camout_for_breath(br, target)
 
     def _plot_single_random_sequence(self, patho):
         target = {'ards': 1, 'non_ards': 0}[patho]
-        self.gt.index = range(len(self.gt))
         patho_idxs = self.gt[self.gt.y == target].index
-        seq_idx = random.choice(patho_idxs)
+        abs_idx = random.choice(patho_idxs)
         br_idx = random.randint(0, self.batch_size-1)
-        cam_outputs, br = self.get_single_sequence_grad_cam(seq_idx, br_idx, target)
+
+        rel_idx = list(self.data.kfold_indexes).index(abs_idx)
+        cam_outputs, br = self.get_single_sequence_grad_cam(rel_idx, br_idx, target)
         self.plot_sequence(br[0], cam_outputs)
         plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
         plt.yticks(fontsize='x-small')
-        return seq_idx, br_idx
+        return abs_idx, br_idx
 
     def _finalize_multi_plot_graph(self, title):
         fig = plt.gcf()