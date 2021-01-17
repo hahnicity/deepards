9aca3a98f24c4b25130214588e3a36c38d28cab1
Gregory Rehm <grehm87@gmail.com>
2021-01-17 01:44:45 -0800

    final update for docker jetson instructions

--- deepards/evaluate.py
+++ deepards/evaluate.py
@@ -2,6 +2,8 @@ import argparse
 
 import pandas as pd
 from pathlib import Path
+from prettytable import PrettyTable
+from sklearn.metrics import accuracy_score, roc_auc_score
 import torch
 from torch.utils.data import DataLoader
 
@@ -23,13 +25,28 @@ def main():
     test_dataset = ARDSRawDataset.make_test_dataset_if_kfold(dataset)
     cls = network_map[args.network](args)
     saved_model_dir = Path(__file__).parent.joinpath('saved_models/{}'.format(args.experiment_name))
+    device = torch.device('cuda:0')
 
     for fold in range(5):
         test_dataset.set_kfold_indexes_for_fold(fold)
         test_loader = DataLoader(test_dataset, 16, True)
-        device = torch.device('cuda:0')
-        model = torch.load(str(saved_model_dir.joinpath(args.models[fold]))).to(device)
-        cls.run_test_epoch(0, model, test_dataset, test_loader, fold)
+        for i, model_name in enumerate(args.models[fold]):
+            model = torch.load(str(saved_model_dir.joinpath(model_name))).to(device)
+            # this is a bit of a hack to be able to aggregate across multiple different
+            # runs (having each model be an individual epoch)
+            cls.run_test_epoch(i, model, test_dataset, test_loader, fold)
+
+    print('\nMean Results')
+    table = PrettyTable()
+    table.field_names = ['Fold', 'Accuracy', 'AUC']
+    for i, fold_df in cls.results.results.groupby('fold_num'):
+        accuracy = round(accuracy_score(fold_df.patho.tolist(), fold_df.prediction.tolist()), 4)
+        auc = round(roc_auc_score(fold_df.patho.tolist(), fold_df.pred_frac.tolist()), 4)
+        table.add_row([i, accuracy, auc])
+    print(table)
+
+    print("\nAggregated Results")
+    cls.results.aggregate_classification_results()
 
 
 if __name__ == "__main__":
